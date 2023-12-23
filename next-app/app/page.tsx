2376a9f60bb7f0063be6c09d83b92dae66d9eb58
JustLenard <justlenard.justme@gmail.com>
2023-12-23 22:35:31 +0200

    Worked on the front page

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,10 +1,26 @@
 import Link from 'next/link'
 import AppHeader from './components/Header'
+import { Button, Typography } from '@mui/material'
 
 const WelcomePage = () => {
 	return (
 		<div>
-			<Link href={'/create-cover-letter'}>Click here to get started</Link>
+			<Typography variant="h3"> Ai Cover letter creator</Typography>
+			<Typography variant="body2" mb={'1rem'}>
+				Are you tired of creating cover letters over and over again? Me too! So I created
+				this website to make it easier to create them using Ai in 3 easy steps
+			</Typography>
+
+			<Typography variant="body2">{"Don't waste time again writing them"}</Typography>
+
+			<Button
+				variant="contained"
+				sx={{
+					mt: '2rem',
+				}}
+			>
+				<Link href={'/create-cover-letter'}>Click here to get started</Link>
+			</Button>
 		</div>
 	)
 }
