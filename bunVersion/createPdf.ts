a5383129c612695d75d8b74bbe762d01dfb4d8d2
JustLenard <justlenard.justme@gmail.com>
2024-02-04 14:55:04 +0200

    Create pdf file with the cover letter

--- bunVersion/createPdf.ts
+++ bunVersion/createPdf.ts
@@ -1,34 +1,50 @@
-// import { PDFDocument, rgb } from
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib'
 
-import { PDFDocument, rgb, StandardFonts } from 'pdf-lib'
-
-// Create a new PDF document
+// Create a new PDFDocument
 const pdfDoc = await PDFDocument.create()
 
-// Add a page to the document
-const page = pdfDoc.addPage([600, 400]) // Specify the page size (width x height)
+// Embed the Times Roman font
+const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
 
-console.log('This is PDFDocument.StandardFonts', PDFDocument)
+// Add a blank page to the document
+const page = pdfDoc.addPage()
 
-// Create a font
-const font = await pdfDoc.embedFont(StandardFonts.Helvetica)
+// Get the width and height of the page
+const { width, height } = page.getSize()
+console.log('This is height', height)
 
-// Add text to the page
-const text = 'Hello, PDF World!'
-page.drawText(text, {
-	x: 50,
-	y: 350,
-	size: 30,
-	font: font,
-	color: rgb(0, 0, 0), // Black color
+console.log('This is width', width)
+
+const coverLetterText = await Bun.file('./coverLetters/coverLetter-1.txt').text()
+
+const res = coverLetterText.replaceAll('\n', ' \n')
+
+res.split('').map((el) => {
+	if (el === '\n') {
+		console.log(true)
+	}
 })
 
-// Serialize the PDF to bytes
+// Draw a string of text toward the top of the page
+const fontSize = 10
+page.drawText(res, {
+	x: 50,
+	y: height - 4 * fontSize,
+	size: fontSize,
+	lineHeight: 12,
+	// font: timesRomanFont,
+	// color: rgb(0, 0.53, 0.71),
+
+	maxWidth: 500,
+	wordBreaks: [' '],
+})
+
+// Serialize the PDFDocument to bytes (a Uint8Array)
 const pdfBytes = await pdfDoc.save()
 
-// Now, you can save the bytes to a file or send them as a response
-// For example, if you want to save it to a file:
-const fs = require('fs')
-fs.writeFileSync('example.pdf', pdfBytes)
+Bun.write('this.pdf', pdfBytes)
 
-console.log('PDF created successfully!')
+// For example, `pdfBytes` can be:
+//   • Written to a file in Node
+//   • Downloaded from the browser
+//   • Rendered in an <iframe>
